51a5784631384af8408ced756db6636b8d0f0088
Raineetu <raineetu0070@gmail.com>
2025-05-22 15:58:28 +0545

    Generic part in typescript

--- Project1/dist/index.js
+++ Project1/dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hello");
+const getUserName = document.querySelector('#user');
+const formSubmit = document.querySelector(".form");
+const main_container = document.querySelector(".main-container");
+//resuable function
+async function myCustomFetcher(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`Network response was not ok,${response.status}`);
+    }
+    const data = response.json();
+    return data;
+}
+function fetchUserData(url) {
+    myCustomFetcher(url, {});
+}
+//default function call
+fetchUserData("https://api.github.com/users");
--- Project1/src/index.ts
+++ Project1/src/index.ts
@@ -1,1 +1,33 @@
-console.log("hello")
+const getUserName = document.querySelector('#user') as HTMLInputElement;
+const formSubmit:HTMLFormElement | null = document.querySelector(".form");
+const main_container = document.querySelector(".main-container") as HTMLElement;
+
+//lets define the properties of an object
+
+interface UserData{
+    id: number,
+    login:string,
+    avatar_url:string,
+    location: string,
+    url:string,
+}
+
+//resuable function
+async function myCustomFetcher<T>(url:string, options?:RequestInit):Promise<T>{
+    const response = await fetch(url, options)
+    if(!response.ok){
+        throw new Error(
+            `Network response was not ok,${response.status}`
+        );
+    }
+    const data = response.json();
+    return data;
+
+}
+
+function fetchUserData(url:string){
+    myCustomFetcher<UserData[]>(url,{});
+}
+
+//default function call
+fetchUserData("https://api.github.com/users");
