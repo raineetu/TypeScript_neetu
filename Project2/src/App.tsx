b027b61146699301a5ec43438a509dd061aabec0
Raineetu <raineetu0070@gmail.com>
2025-05-27 21:07:42 +0545

    Quiz app initialized

--- Project2/src/App.tsx
+++ Project2/src/App.tsx
@@ -1,18 +1,21 @@
 import './App.css'
-import Person from './components/Person';
+// import Person from './components/Person';
 
 function App() {
-  const data = {
-    name: "Neetu",
-    age : 22,
-    isMarried:  false,
+  // const data = {
+  //   name: "Neetu",
+  //   age : 22,
+  //   isMarried:  false,
 
-  }
+  // }
   
   return (
     <>
-      <h1>{data.name}</h1>
-      <Person name={data.name} age = {data.age} isMarried={data.isMarried} />
+      {/* <h1>{data.name}</h1>
+      <Person name={data.name} age = {data.age} isMarried={data.isMarried} /> */}
+      <div className='App'>
+          Quiz
+      </div>
     </>
   );
 }
